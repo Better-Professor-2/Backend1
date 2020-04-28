dcf583f73a10c13d819e5ce2e5f3e405d69e00d6
Shamskol <koladejun@yahoo.com>
2020-04-28 18:24:59 +0100

    Modified the migrations files and merged all the tables into a file

--- database/migrations/20200428175035__create_tables.js
+++ database/migrations/20200428175035__create_tables.js
@@ -1,16 +1,25 @@
 
 exports.up = function(knex) {
-    return knex.schema.createTable('professors', tbl =>{
+    return knex.schema
+    .createTable('Users', tbl => {
+      tbl.increments();
+      tbl.string('username', 128)
+        .notNullable()
+        .unique();
+      tbl.string('password', 128)
+        .notNullable();
+    })
+    .createTable('professors', tbl =>{
         tbl.increments("id");
         tbl.string("first_name", 255).notNullable();
         tbl.string("last_name", 255).notNullable();
         tbl.string("email", 255).notNullable();
         tbl.string("password", 255).notNullable();
     })
+  };
   
-};
+
 
 exports.down = function(knex) {
-    return knex.schema.dropTableIfExists(`professors`);
   
 };
